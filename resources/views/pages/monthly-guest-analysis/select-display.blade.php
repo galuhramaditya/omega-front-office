dcf408496b2bf2e20253ebfb3da267f2ad305653
galuh ramaditya <galuh.ramaditya@gmail.com>
2019-11-05 13:29:04 +0700

    seelct

--- resources/views/pages/monthly-guest-analysis/select-display.blade.php
+++ resources/views/pages/monthly-guest-analysis/select-display.blade.php
@@ -1,14 +1,14 @@
 <div class="row display-hide" id="select-display">
     <div class="col-xs-6 text-center">
         <div class="btn-group">
-            <button class="btn red" v-on:click.prevent="report.handle_display({based_on : 'player_status'})"><i class="fa fa-user-o"></i> Based on Player Status</button>
-            <button class="btn green-jungle" v-on:click.prevent="report.handle_display({based_on : 'gender'})"><i class="fa fa-venus-mars"></i> Based on Gender</button>
+            <button class="btn red" v-on:click.prevent="report.handle_display({based_on : 'player_status', by : 'player'})"><i class="fa fa-user-o"></i> Number by Player Status</button>
+            <button class="btn green-jungle" v-on:click.prevent="report.handle_display({based_on : 'player_status', by : 'amount'})"><i class="fa fa-user-o"></i> Amount by Player Status</button>
         </div>
     </div>
     <div class="col-xs-6 text-center">
         <div class="btn-group">
-            <button class="btn grey-mint" v-on:click.prevent="report.handle_display({by : 'player'})"><i class="fa fa-arrows-h"></i> By Number of Player</button>
-            <button class="btn purple" v-on:click.prevent="report.handle_display({by : 'amount'})"><i class="fa fa-arrows-v"></i> By Amount</button>
+            <button class="btn grey-mint" v-on:click.prevent="report.handle_display({based_on : 'gender', by : 'player'})"><i class="fa fa-venus-mars"></i> Number by Gender</button>
+            <button class="btn purple" v-on:click.prevent="report.handle_display({based_on : 'gender', by : 'amount'})"><i class="fa fa-venus-mars"></i> Amount by Gender</button>
         </div>
     </div>
 </div>
