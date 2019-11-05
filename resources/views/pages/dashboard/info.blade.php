ca20f92b37aa899466a226bf0ac839231a9ba8f9
galuh ramaditya <galuh.ramaditya@gmail.com>
2019-11-05 12:35:58 +0700

    dashboard

--- resources/views/pages/dashboard/info.blade.php
+++ resources/views/pages/dashboard/info.blade.php
@@ -7,43 +7,25 @@
                     <span class="caption-subject font-green bold uppercase">User Info</span>
                 </div>
             </div>
-            <div class="portlet-body" v-if="user != null">
-                <div class="row">
-                    <div class="col-xs-5 text-right">
-                        Company Name
-                    </div>
-                    <div class="col-xs-1" style="width: 1%"> : </div>
-                    <div class="col-xs-6 bold">
-                        @{{user.conm}}
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-xs-5 text-right">
-                        Username
-                    </div>
-                    <div class="col-xs-1" style="width: 1%"> : </div>
-                    <div class="col-xs-6 bold">
-                        @{{user.username}}
-                    </div>
-                </div>
-                <div class="row" v-if="user.role != null">
-                    <div class="col-xs-5 text-right">
-                        Role
-                    </div>
-                    <div class="col-xs-1" style="width: 1%"> : </div>
-                    <div class="col-xs-6 bold">
-                        @{{user.role.name}}
-                    </div>
-                </div>
-                <div class="row" v-if="user.role != null">
-                    <div class="col-xs-5 text-right">
-                        Level
-                    </div>
-                    <div class="col-xs-1" style="width: 1%"> : </div>
-                    <div class="col-xs-6 bold">
-                        @{{user.role.level}}
-                    </div>
-                </div>
+            <div class="portlet-body table-responsive table-scrollable" style="border: 0px" v-if="user != null">
+                <table class="table">
+                    <tr>
+                        <td class="text-right" style="width: 50%">Company Name</td>
+                        <td class="bold" style="width: 50%">@{{user.conm}}</td>
+                    </tr>
+                    <tr>
+                        <td class="text-right">Username</td>
+                        <td class="bold">@{{user.username}}</td>
+                    </tr>
+                    <tr>
+                        <td class="text-right">Role</td>
+                        <td class="bold">@{{user.role.name}}</td>
+                    </tr>
+                    <tr>
+                        <td class="text-right">Level</td>
+                        <td class="bold">@{{user.role.level}}</td>
+                    </tr>
+                </table>
             </div>
         </div>
     </div>
